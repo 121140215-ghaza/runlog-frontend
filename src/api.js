6c26158ddae66f7c09586bd9f6a602a87109fab2
121140215-ghaza <ghaza.121140215@student.itera.ac.id>
2025-05-22 16:16:31 +0700

    update api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://0fe8-114-10-101-187.ngrok-free.app/api', // Pastikan backend berjalan di localhost:6543
+  baseURL: 'https://5551-180-241-18-26.ngrok-free.app/api', // Pastikan backend berjalan di localhost:6543
   withCredentials: true,  // Mengirimkan cookies jika perlu
 });
 
